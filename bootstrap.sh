da756833dc3c9818bc0e3ad2e2df6a240cd1ce02
XiaoLong Chen <xiaolong.chen01@sap.com>
2017-08-27 14:31:04 +0800

    modified

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,140 +1,140 @@
 #!/usr/bin/env bash
 
-echo 'Update packages list...'
-echo "------------------------"
-apt-get -y update
-
-echo 'Install Xubuntu Desktop & co...'
-echo "------------------------"
-export DEBIAN_FRONTEND=noninteractive
-apt-get -y --force-yes --no-install-recommends install xubuntu-desktop mousepad xubuntu-icon-theme \
-xfce4-goodies xubuntu-wallpapers gksu cifs-utils xfce4-whiskermenu-plugin firefox \
-xarchiver filezilla synaptic curl vim wget
-
-echo 'Install VB addon and x11 display'
-sudo apt-get -y --force-yes --no-install-recommends install virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms
-
-echo 'Set New York timezone...'
-echo "------------------------"
-echo "America/New_York" | sudo tee /etc/timezone
-sudo dpkg-reconfigure --frontend noninteractive tzdata
-
-echo 'Set English keyboard layout...'
-echo "------------------------"
-sudo sed -i 's/XKBLAYOUT="us"/g' /etc/default/keyboard
-
-echo 'Install Chrome...'
-echo "------------------------"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp
-sudo dpkg -i /tmp/google-chrome*
-sudo apt-get -f install -y
-rm /tmp/google*chrome*.deb
-
-echo 'Install JDK 8 in /usr/lib/jvm/java-8-oracle...'
-echo "------------------------"
-sudo add-apt-repository ppa:webupd8team/java -y
-sudo apt-get update -y
-sudo echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-sudo echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-sudo apt-get install -y oracle-jdk8-installer -y
-sudo apt-get install oracle-java8-set-default -y
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
-export PATH=$JAVA_HOME/bin:$PATH
-
-# echo 'Install JDK 6 in /usr/lib/jvm/java-6-oracle...'
-# sudo apt-get install -y oracle-java6-installer
-
-echo 'Create Development directory...'
-echo "------------------------"
-mkdir /home/vagrant/Development
-mkdir /home/vagrant/Development/git
-sudo chmod 777 -R /home/vagrant/Development/
-
-echo 'Install Spring Tool Suite based on Eclipse 4.6...'
-echo "------------------------"
-# wget http://eclipse.mirror.garr.it/mirrors/eclipse//technology/epp/downloads/release/kepler/SR2/eclipse-jee-kepler-SR2-linux-gtk-x86_64.tar.gz -P /tmp
-wget http://dist.springsource.com/release/STS/3.7.3.RELEASE/dist/e4.6/spring-tool-suite-3.7.3.RELEASE-e4.6-linux-gtk-x86_64.tar.gz -P /tmp
-tar xzf /tmp/spring-tool-suite-*-linux-gtk*.tar.gz -C /home/vagrant/Development/
-# sudo ln -s /home/vagrant/Development/sts-bundle/sts-3.7.0.RELEASE /usr/bin/sts
-# sudo ln -s /home/vagrant/Development/eclipse/eclipse /usr/bin/eclipse
-# wget -N https://raw.github.com/lfiammetta/vagrant/master/settings/xubuntu/eclipse.desktop -P /tmp
-# sudo mv /tmp/eclipse.desktop /usr/share/applications/
-# rm /tmp/eclipse-jee-*-linux-gtk*.tar.gz
-
-echo 'Install Tomcat 8.0.35...'
-echo "------------------------"
-# wget http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.39/bin/apache-tomcat-7.0.39.tar.gz -P /tmp
-wget http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.35/bin/apache-tomcat-8.0.35.tar.gz -P /tmp
-tar xzf /tmp/apache-tomcat*.tar.gz -C /home/vagrant/Development/
-rm /tmp/apache-tomcat*.tar.gz
-
-# http://localhost/
-echo 'Install Apache2...'
-echo "------------------------"
-sudo apt-get install apache2 -y
-
-# echo 'Install PHP 5...'
+# echo 'Update packages list...'
 # echo "------------------------"
-# sudo apt-get install php5 -y
+# apt-get -y update
 
-# echo 'Install MySql...'
+# echo 'Install Xubuntu Desktop & co...'
 # echo "------------------------"
-# sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-# sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-# export DEBIAN_FRONTEND=noninteractive && sudo apt-get -q -y install mysql-server
+# export DEBIAN_FRONTEND=noninteractive
+# apt-get -y --force-yes --no-install-recommends install xubuntu-desktop mousepad xubuntu-icon-theme \
+# xfce4-goodies xubuntu-wallpapers gksu cifs-utils xfce4-whiskermenu-plugin firefox \
+# xarchiver filezilla synaptic curl vim wget
 
-# http://localhost/phpmyadmin/
-# echo 'Install phpMyAdmin...'
+# echo 'Install VB addon and x11 display'
+# sudo apt-get -y --force-yes --no-install-recommends install virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms
+
+# echo 'Set New York timezone...'
 # echo "------------------------"
-# sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
-# sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/app-password-confirm password root'
-# sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password root'
-# sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password root'
-# sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2'
-# export DEBIAN_FRONTEND=noninteractive && sudo apt-get -q -y install phpmyadmin
+# echo "America/New_York" | sudo tee /etc/timezone
+# sudo dpkg-reconfigure --frontend noninteractive tzdata
 
-echo 'Install DBeaver 2.4.0...'
-echo "------------------------"
-wget -c http://dbeaver.jkiss.org/files/dbeaver_2.4.0_amd64.deb -P /tmp
-sudo dpkg -i /tmp/dbeaver_2.4.0_amd64.deb
-sudo apt-get install -f
-rm /tmp/dbeaver*.deb
-
-echo 'Install Git and create local repository directory'
-echo "------------------------"
-sudo apt-get install git -y
-
-echo 'Install Git Flow...'
-echo "------------------------"
-wget -q – http://github.com/nvie/gitflow/raw/develop/contrib/gitflow-installer.sh –no-check-certificate -P /tmp
-sudo chmod a+x /tmp/gitflow-installer.sh
-sudo sh /tmp/gitflow-installer.sh
-
-# echo 'Install Maven in /usr/share/maven...'
+# echo 'Set English keyboard layout...'
 # echo "------------------------"
-sudo apt-cache search maven
-sudo apt-get install maven -y
+# sudo sed -i 's/XKBLAYOUT="us"/g' /etc/default/keyboard
 
-echo 'Install Oh My Zsh'
-echo "------------------------"
-sudo apt-get install zsh -y
-touch /home/vagrant/.zshrc
-# wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-# sh -c "$(wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo chsh -s /bin/zsh vagrant
-sudo zsh
-sudo chown vagrant:vagrant /home/vagrant/.zshrc
-sudo chown -R vagrant:vagrant /home/vagrant/.oh-my-zsh
+# echo 'Install Chrome...'
+# echo "------------------------"
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp
+# sudo dpkg -i /tmp/google-chrome*
+# sudo apt-get -f install -y
+# rm /tmp/google*chrome*.deb
 
-# Change the oh my zsh default theme.
-mkdir /home/vagrant/workspace
-sudo chown -R vagrant:vagrant /home/vagrant/workspace
-sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
-sudo sed -i 's/plugins=(git)/plugins=(git ruby rails bower bundler docker gem git-extras mvn npm python redis-cli)/g' ~/.zshrc
-git clone https://github.com/powerline/fonts.git /home/vagrant/workspace/powerline
-sh /home/vagrant/workspace/powerline/install.sh
-rm -rf /home/vagrant/workspace/powerline
+# echo 'Install JDK 8 in /usr/lib/jvm/java-8-oracle...'
+# echo "------------------------"
+# sudo add-apt-repository ppa:webupd8team/java -y
+# sudo apt-get update -y
+# sudo echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+# sudo echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+# sudo apt-get install -y oracle-jdk8-installer -y
+# sudo apt-get install oracle-java8-set-default -y
+# export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
+# export PATH=$JAVA_HOME/bin:$PATH
+
+# # echo 'Install JDK 6 in /usr/lib/jvm/java-6-oracle...'
+# # sudo apt-get install -y oracle-java6-installer
+
+# echo 'Create Development directory...'
+# echo "------------------------"
+# mkdir /home/vagrant/Development
+# mkdir /home/vagrant/Development/git
+# sudo chmod 777 -R /home/vagrant/Development/
+
+# echo 'Install Spring Tool Suite based on Eclipse 4.6...'
+# echo "------------------------"
+# # wget http://eclipse.mirror.garr.it/mirrors/eclipse//technology/epp/downloads/release/kepler/SR2/eclipse-jee-kepler-SR2-linux-gtk-x86_64.tar.gz -P /tmp
+# wget http://dist.springsource.com/release/STS/3.7.3.RELEASE/dist/e4.6/spring-tool-suite-3.7.3.RELEASE-e4.6-linux-gtk-x86_64.tar.gz -P /tmp
+# tar xzf /tmp/spring-tool-suite-*-linux-gtk*.tar.gz -C /home/vagrant/Development/
+# # sudo ln -s /home/vagrant/Development/sts-bundle/sts-3.7.0.RELEASE /usr/bin/sts
+# # sudo ln -s /home/vagrant/Development/eclipse/eclipse /usr/bin/eclipse
+# # wget -N https://raw.github.com/lfiammetta/vagrant/master/settings/xubuntu/eclipse.desktop -P /tmp
+# # sudo mv /tmp/eclipse.desktop /usr/share/applications/
+# # rm /tmp/eclipse-jee-*-linux-gtk*.tar.gz
+
+# echo 'Install Tomcat 8.0.35...'
+# echo "------------------------"
+# # wget http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.39/bin/apache-tomcat-7.0.39.tar.gz -P /tmp
+# wget http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.35/bin/apache-tomcat-8.0.35.tar.gz -P /tmp
+# tar xzf /tmp/apache-tomcat*.tar.gz -C /home/vagrant/Development/
+# rm /tmp/apache-tomcat*.tar.gz
+
+# # http://localhost/
+# echo 'Install Apache2...'
+# echo "------------------------"
+# sudo apt-get install apache2 -y
+
+# # echo 'Install PHP 5...'
+# # echo "------------------------"
+# # sudo apt-get install php5 -y
+
+# # echo 'Install MySql...'
+# # echo "------------------------"
+# # sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+# # sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+# # export DEBIAN_FRONTEND=noninteractive && sudo apt-get -q -y install mysql-server
+
+# # http://localhost/phpmyadmin/
+# # echo 'Install phpMyAdmin...'
+# # echo "------------------------"
+# # sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
+# # sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/app-password-confirm password root'
+# # sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password root'
+# # sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password root'
+# # sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2'
+# # export DEBIAN_FRONTEND=noninteractive && sudo apt-get -q -y install phpmyadmin
+
+# echo 'Install DBeaver 2.4.0...'
+# echo "------------------------"
+# wget -c http://dbeaver.jkiss.org/files/dbeaver_2.4.0_amd64.deb -P /tmp
+# sudo dpkg -i /tmp/dbeaver_2.4.0_amd64.deb
+# sudo apt-get install -f
+# rm /tmp/dbeaver*.deb
+
+# echo 'Install Git and create local repository directory'
+# echo "------------------------"
+# sudo apt-get install git -y
+
+# echo 'Install Git Flow...'
+# echo "------------------------"
+# wget -q – http://github.com/nvie/gitflow/raw/develop/contrib/gitflow-installer.sh –no-check-certificate -P /tmp
+# sudo chmod a+x /tmp/gitflow-installer.sh
+# sudo sh /tmp/gitflow-installer.sh
+
+# # echo 'Install Maven in /usr/share/maven...'
+# # echo "------------------------"
+# sudo apt-cache search maven
+# sudo apt-get install maven -y
+
+# echo 'Install Oh My Zsh'
+# echo "------------------------"
+# sudo apt-get install zsh -y
+# touch /home/vagrant/.zshrc
+# # wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+# # sh -c "$(wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sudo chsh -s /bin/zsh vagrant
+# sudo zsh
+# sudo chown vagrant:vagrant /home/vagrant/.zshrc
+# sudo chown -R vagrant:vagrant /home/vagrant/.oh-my-zsh
+
+# # Change the oh my zsh default theme.
+# mkdir /home/vagrant/workspace
+# sudo chown -R vagrant:vagrant /home/vagrant/workspace
+# sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+# sudo sed -i 's/plugins=(git)/plugins=(git ruby rails bower bundler docker gem git-extras mvn npm python redis-cli)/g' ~/.zshrc
+# git clone https://github.com/powerline/fonts.git /home/vagrant/workspace/powerline
+# sh /home/vagrant/workspace/powerline/install.sh
+# rm -rf /home/vagrant/workspace/powerline
 
 echo 'Install docker'
 # Ask for the user password
